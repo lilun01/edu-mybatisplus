34245ce88b702c99d32eaf88320739ba66114eb8
lilun <lilun@offerxiansheng.com>
2020-05-29 11:31:57 +0800

    优化注入

--- edu-parent/edu-web/src/main/java/com/nature/edu/controller/OrderController.java
+++ edu-parent/edu-web/src/main/java/com/nature/edu/controller/OrderController.java
@@ -3,32 +3,42 @@ package com.nature.edu.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.nature.edu.feign.OrderFeignClient;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
-	@Autowired
+	@Resource
 	private OrderFeignClient orderFeignClient;
+	
+	@Resource
+	private UserController userController;
 
+
+	@SentinelResource(value = "create", blockHandler  = "execptionHandler")
 	@RequestMapping("/create")
 	public String create() {
 		long startTime = System.currentTimeMillis();
-		 boolean result = orderFeignClient.create("U100000", "C100000", 30);
-		 long endTime = System.currentTimeMillis();
-		float f =  (endTime-startTime)/1000f;
-		 System.out.println("调用结果="+result+",耗时："+(endTime-startTime)/1000f+"s");
-		return result+",耗时："+f;
-	}
-	@RequestMapping("/create2")
-	public boolean create2() {
-		long startTime = System.currentTimeMillis();
-		boolean result = orderFeignClient.create2( );
+		boolean result = orderFeignClient.create("U100000", "C100000", 30);
 		long endTime = System.currentTimeMillis();
-		System.out.println("调用结果="+result+",耗时："+(endTime-startTime)/1000f+"s");
-		return result;
+		float f = (endTime - startTime) / 1000f;
+		System.out.println("调用结果=" + result + ",耗时：" + (endTime - startTime) / 1000f + "s");
+		return result + ",耗时：" + f;
 	}
+
+	
+	
+	@RequestMapping("/create2")
+	public String create2() {
+		String s = userController.testSentinel(null);
+		return s;
+	}
+
+	 
+	
 
 }
